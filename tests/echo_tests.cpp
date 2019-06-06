60410e21b108e3fc2a1231a6e32c4ee2ea6d37c8
Tegan Lamoureux <Tegan.Lamoureux@digimarc.com>
2019-06-06 15:41:46 -0700

    Oops, forgot echo tests.

---
+++ tests/echo_tests.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include <vector>
+#include <cstdlib>
+
+#include <jack/jack.h>
+#include <jack/ringbuffer.h>
+
+#include "echo.h"
+
+using namespace std;
+
+namespace {
+
+class EchoTests : public ::testing::Test {
+protected:
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
+
+    static jack_ringbuffer_t *buffer;
+};
+
+jack_ringbuffer_t* EchoTests::buffer;
+
+
+TEST_F(EchoTests, can_use_echo) {
+    vector<double> test;
+	Echo::AddEcho_FIR(test, 0.0, 0.0, 0.0, 0.0);
+}
+
+}
+
